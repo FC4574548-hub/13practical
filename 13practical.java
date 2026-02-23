b65850f9ae13b85389ab04d0b161ae714b6bf1f8
FC4574548-hub <4574548@myuwc.ac.za>
2026-02-23 15:53:27 +0200

    Did more error checking

--- 13practical.java
+++ 13practical.java
@@ -1,24 +1,21 @@
 import java.lang.Math.*;   import java.io.*;   import java.text.*; import java.util.Random;
 
 class Node{
-  int key;
-  String data;
-  
-  Node(int k, String d){
-    key = k;
-    data = d;
-  }
+    int key;
+    String data;
+
+    Node(int k, String d){
+        key = k;
+        data = d;
+    }
 }
 
-public class timeMethods{
-  public static int N = 32654;
-  static Node array[] = new Node[N];
-
-    
-      
+public class timeMethods {
+    public static int N = 32654;
+    static Node array[] = new Node[N];
 
 
-    public static void main(String args[]) throws Exception{
+    public static void main(String args[]) throws Exception {
 
         DecimalFormat twoD = new DecimalFormat("0.00");
         DecimalFormat fourD = new DecimalFormat("0.0000");
@@ -29,118 +26,121 @@ public class timeMethods{
         double totalTime = 0.0;
         int n = N;
         int repetition, repetitions = 30;
-        
+
         Random random = new Random();
-        
+
         loadFile();
-        
+
         int[] keys = new int[30];
-        for (int i = 0; i < 30; i++){
-          keys[i] = random.nextInt(32654) + 1;
+        for (int i = 0; i < 30; i++) {
+            keys[i] = random.nextInt(32654) + 1;
         }
 
         runTime = 0;
-        for(repetition = 0; repetition < repetitions; repetition++) {
+        for (repetition = 0; repetition < repetitions; repetition++) {
             start = System.currentTimeMillis();
 
             // call the procedures to time here:
             // Linear Search
-            for (int i= 0; i < 30; i++){
-              linearSearch(keys[i]);
+            for (int i = 0; i < 30; i++) {
+                linearSearch(keys[i]);
             }
 
             finish = System.currentTimeMillis();
 
-            time = (double)(finish - start);
+            time = (double) (finish - start);
             runTime += time;
-            runTime2 += (time*time); }
+            runTime2 += (time * time);
+        }
 
-        double aveRuntime = runTime/repetitions;
+        double aveRuntime = runTime / repetitions;
         double stdDeviation =
-                Math.sqrt(runTime2 - repetitions*aveRuntime*aveRuntime)/(repetitions-1);
-                
-             // Binary Search
-        for(repetition = 0; repetition < repetitions; repetition++) {
+                Math.sqrt(runTime2 - repetitions * aveRuntime * aveRuntime) / (repetitions - 1);
+
+        // Binary Search
+        for (repetition = 0; repetition < repetitions; repetition++) {
             start = System.currentTimeMillis();
-            
-            for (int i= 0; i < 30; i++){
-              binarySearch(keys[i]);
+
+            for (int i = 0; i < 30; i++) {
+                binarySearch(keys[i]);
             }
 
             finish = System.currentTimeMillis();
 
-            time = (double)(finish - start);
+            time = (double) (finish - start);
             runTime += time;
-            runTime2 += (time*time); }
+            runTime2 += (time * time);
+        }
 
-        double aveRuntime = runTime/repetitions;
-        double stdDeviation =
-                Math.sqrt(runTime2 - repetitions*aveRuntime*aveRuntime)/(repetitions-1);
-             
+        aveRuntime = runTime / repetitions;
+        stdDeviation =
+                Math.sqrt(runTime2 - repetitions * aveRuntime * aveRuntime) / (repetitions - 1);
+
 
         System.out.printf("\n\n\fStatistics\n");
         System.out.println("________________________________________________");
-        System.out.println("Total time   =           " + runTime/1000 + "s.");
+        System.out.println("Total time   =           " + runTime / 1000 + "s.");
         System.out.println("Total time\u00b2  =           " + runTime2);
-        System.out.println("Average time =           " + fiveD.format(aveRuntime/1000)
+        System.out.println("Average time =           " + fiveD.format(aveRuntime / 1000)
                 + "s. " + '\u00B1' + " " + fourD.format(stdDeviation) + "ms.");
         System.out.println("Standard deviation =     " + fourD.format(stdDeviation));
         System.out.println("n            =           " + n);
-        System.out.println("Average time / run =     " + fiveD.format(aveRuntime/n*1000)
+        System.out.println("Average time / run =     " + fiveD.format(aveRuntime / n * 1000)
                 + '\u00B5' + "s. ");
 
         System.out.println("Repetitions  =             " + repetitions);
         System.out.println("________________________________________________");
         System.out.println();
-        System.out.println(); }	}
-
-    static void loadFile() throws Exception{
-      BufferedReader br = new BufferedReader(new FileReader(ulysses.numbered));
-      String line;
-      int i = 0;
-      while ((line=br.readLine())!=null){
-        String parts[] = line.split("",2);
-        
-        int key = integer.parseInt(parts[0]);
-        
-        String data = parts[1];
-        
-        array[i] = new Node(key,data);
-        
-        i++;
-      }
-      br.close();
+        System.out.println();
     }
- 
+
+
+    static void loadFile() throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader("ulysses.numbered"));
+        String line;
+        int i = 0;
+        while ((line = br.readLine()) != null) {
+            String parts[] = line.split("", 2);
+
+            int key = Integer.parseInt(parts[0]);
+
+            String data = parts[1];
+
+            array[i] = new Node(key, data);
+
+            i++;
+        }
+        br.close();
+    }
+
+
+    public static int linearSearch(int key) {
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].key == key) {
+                return 1;
+            }}
+            return -1;
+        }
     
-    public static int linearSearch(int key){
 
-        for(int i=0;i< array.length;i++){
-        if(arr[i].key == key){
-        return 1;
-        }
-        return -1;
+
+    public static int binarySearch(int key) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (array[mid].key == key) {
+                return mid;
+            } else if (key < array[mid].key) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }}
+            return -1;
         }
     }
-        
-
-    public static int binarySearch(int key){
-            int left = 0;
-            int right = array.length-1;
-            while (left <= right) {
-                int mid = (low+high)/2;
-                
-                if (array[mid].key == key){
-                  return mid;
-                } else if (key < array[mid].key) {
-                  high = mid-1;
-                } else {
-                  low = mid+1;
-                }
-                return -1;
-        }
-        }
-        }
-        
 
         
+
